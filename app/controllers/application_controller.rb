f67280d96662c52a48e1c480dd77083fc96e2304
Alyssa Levitz <alyssal@yammer-inc.com>
2013-08-10 14:36:59 -0700

    finished comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-  	@articles = Article.all
-  end
-
-  def show
-  	@article = Article.find(params[:id])
-  end
 end
